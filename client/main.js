f6577650e82f347d65f632531bcb1b2a48bf0aae
Gautam Gahlawat <gautam.gahlawat@gmail.com>
2016-11-17 03:26:46 +0530

    Display todos on view with bootstrap design
    connection to the db and one is able to view the data from the collection to the frontend

--- client/main.js
+++ client/main.js
@@ -4,5 +4,12 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 import { Mongo } from 'meteor/mongo';
  
- 
+/*start from here*/
+
 Todos = new Mongo.Collection('todos');
+//Template Helpers
+Template.main.helpers({
+	todos: function() {
+		return Todos.find();
+	}
+});
